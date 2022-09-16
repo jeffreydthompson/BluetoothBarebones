3c573b504d73b035d74b11d768fd53b6b9d55ad9
Jeffrey Thompson <jefreythompson@gmail.com>
2022-09-16 02:30:48 -0700

    Initial Commit

---
+++ BluetoothBarebones/BluetoothBarebonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothBarebonesApp.swift
+//  BluetoothBarebones
+//
+//  Created by Jeffrey Thompson on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothBarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothBarebones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BluetoothBarebones
+//
+//  Created by Jeffrey Thompson on 9/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
